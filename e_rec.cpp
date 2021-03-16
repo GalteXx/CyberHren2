ea15f1782bed4764cded64774fb80e1bf4bb6354
ujuj04 <66685900+ujuj04@users.noreply.github.com>
2021-03-16 17:14:47 +0300

    Add files via upload

--- e_rec.cpp
+++ e_rec.cpp
@@ -1,20 +1,13 @@
 #include "base.h"
 
-void e_rec(int w, int h, char b_c1)
+void e_rec(int x_r, int y_r, int w, int h, char b_c, vector<vector<char> >& arr)
 {
-    int x, y, w1, h1;
-    char b_c2;
-    cin >> x >> y >> w1 >> h1 >> b_c2;
-    char arr[w][h];
     for (int i = 0; i < w; i++)
     {
         for (int j = 0; j < h; j++)
         {
             if (i == y && j < w1 + x && j >= x || i == y + h1 - 1 && j < w1 + x && j >= x || j == x && i < h1 + y && i >= y || j == x + w1 - 1 && i < h1 + y && i >= y)
-                cout << b_c2 << " ";
-            else
-                cout << b_c1 << " ";
+                arr[i][j] = b_c;
         }
-        cout << endl;
     }
 }
