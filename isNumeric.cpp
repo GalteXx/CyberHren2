010121280459cb7f3122b60d55678239c30a40ab
GalteXx <65533265+GalteXx@users.noreply.github.com>
2021-03-16 22:12:29 +0300

    damn .o

--- isNumeric.cpp
+++
@@ -1,9 +0,0 @@
-#include "base.h"
-
-bool isNumeric(char a)
-{
-	if(a <= 57 || a >= 48 )
-		return true;
-	
-	return false;
-}
--- mod.cpp
+++
@@ -1,8 +0,0 @@
-#include "base.h"
-
-int mod(int a)
-{
-    if(a < 0)
-        return -a;
-    return a;
-}
