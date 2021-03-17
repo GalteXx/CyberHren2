5b5bdb3dce9cf190db56b051b5a7bf5c28a0e40c
ujuj04 <66685900+ujuj04@users.noreply.github.com>
2021-03-17 16:23:43 +0300

    Add files via upload

--- e_rec.cpp
+++ e_rec.cpp
@@ -1,13 +1,12 @@
 #include "base.h"
-
-void e_rec(int x_r, int y_r, int w, int h, char b_c, vector<vector<char> >& arr)
+void e_rec(int x, int y, int w, int h, char b_c, vector<vector<char> >& arr)
 {
-    for (int i = 0; i < w; i++)
-    {
-        for (int j = 0; j < h; j++)
-        {
-            if (i == y && j < w1 + x && j >= x || i == y + h1 - 1 && j < w1 + x && j >= x || j == x && i < h1 + y && i >= y || j == x + w1 - 1 && i < h1 + y && i >= y)
-                arr[i][j] = b_c;
-        }
-    }
+    for (int i = x; i < x + w; i++) // верх
+        arr[y][i] = b_c;
+    for (int i = y + 1; i < y + h - 1; i++) // лево
+        arr[i][x] = b_c;
+    for (int i = y + 1; i < y + h - 1; i++) // право
+    	arr[i][x + w - 1] = b_c;
+    for (int i = x; i < x + w; i++) // низ
+    	arr[y + h - 1][i] = b_c;
 }
